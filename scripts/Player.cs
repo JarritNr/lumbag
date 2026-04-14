ad8abbe9e2164444f1433f16f939b92f912c19ab
Jarrit <neuschaeferj@gmail.com>
2026-04-14 21:59:40 +0200

    animation logic

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -1,19 +1,41 @@
 using Godot;
 using System;
+using System.Runtime.CompilerServices;
 
 public partial class Player : CharacterBody2D
 {
-    [Export]
-    public int Speed {get;set;} = 100;
-    public void getInput()
-    {
-        Vector2 InputDirection = Input.GetVector("left", "right", "up", "down");
-        Velocity = InputDirection * Speed;
-    }
+    [Export] public int Speed {get;set;} = 100;
+    [Export] public AnimatedSprite2D _playerSprite;
 
     public override void _PhysicsProcess(double delta)
     {
-        getInput();
+        Vector2 InputDirection = Input.GetVector("left", "right", "up", "down");
+        Velocity = InputDirection * Speed;
         MoveAndSlide();
+        update_animation(InputDirection);
+    }
+
+    public void update_animation(Vector2 direction)
+    {
+        if(direction != Vector2.Zero)
+        {
+            if(Mathf.Abs(direction.X) > 0)
+            {
+                _playerSprite.FlipH = direction.X > 0;
+                _playerSprite.Play("walk_s");
+            }
+            else if(direction.Y < 0) _playerSprite.Play("walk_u");
+            else if(direction.Y > 0) _playerSprite.Play("walk_d");
+        }
+        else
+        {
+            String currentAnimation = _playerSprite.Animation;
+
+            if (!currentAnimation.StartsWith("idle_"))
+            {
+                String suffix = currentAnimation.Substring(currentAnimation.Length - 1);
+                _playerSprite.Play("idle_" + suffix);
+            }
+        }
     }
 }
