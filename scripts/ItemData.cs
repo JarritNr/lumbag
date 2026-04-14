ffe0e9ee00f9ed507d9b06f4663be1fa67417517
Jarrit <neuschaeferj@gmail.com>
2026-04-14 23:07:40 +0200

    Itemdata script initialisation

---
+++ scripts/ItemData.cs
@@ -0,0 +1,8 @@
+using Godot;
+
+public partial class ItemData : Resource
+{
+    [Export] public string Name {get; set;} = "Item";
+    [Export] public Texture2D Icon {get; set;}
+    [Export] public bool IsStackable {get; set;} = false;
+}
